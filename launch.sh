b19b1ac7b33a0cd8cf98c7434cecb6e9904504d8
ownssec <ownssec@gmail.com>
2025-07-20 10:20:58 +0800

    update launch sh

--- launch.sh
+++ launch.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Terminate already running bar instances
+# Ensure environment is ready
+export DISPLAY=:0
+export XDG_CURRENT_DESKTOP=XFCE
+
+# Kill existing Polybar instances
 killall -q polybar
 
-# Wait until they are shut down
+# Wait until all Polybar processes have exited
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# Small delay to allow XFCE panel to finish loading
+sleep 2
+
 # Launch Polybar
-polybar example &
+polybar --config="$HOME/.config/polybar/config.ini" example &
+
+
