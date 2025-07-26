b4c0213a8965bf9e2b2a52bef48dca1018494e6e
ownssec <ownssec@gmail.com>
2025-07-26 16:40:33 +0800

    update change icon

--- battery-status.sh
+++ battery-status.sh
@@ -10,13 +10,12 @@ fi
 capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 
 if [ "$capacity" -lt 30 ]; then
-  echo "%{F#ff5555}▂"
+  echo "%{F#ff5555}▮▯▯▯"
 elif [ "$capacity" -lt 60 ]; then
-  echo "%{F#e0e0e0}▄"
+  echo "%{F#e0e0e0}▮▮▯▯"
 elif [ "$capacity" -lt 90 ]; then
-  echo "%{F#e0e0e0}▅"
+  echo "%{F#e0e0e0}▮▮▮▯"
 else
   # Default color (light gray)
-  # echo "%{F#e0e0e0}% ▮▮▮▮ $capacity%%{F-} "
-  echo "%{F#e0e0e0}	▇"
+  echo "%{F#e0e0e0}▮▮▮▮"
 fi
