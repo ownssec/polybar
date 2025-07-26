6875899316c523f46ff6643af238b6ad85beee2e
ownssec <ownssec@gmail.com>
2025-07-26 16:38:48 +0800

    network speed updated and battery icon

--- battery-status.sh
+++ battery-status.sh
@@ -10,13 +10,10 @@ fi
 capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 
 if [ "$capacity" -lt 30 ]; then
-  # Red if battery < 50%
   echo "%{F#ff5555}▂"
 elif [ "$capacity" -lt 60 ]; then
-  # Red if battery < 50%
   echo "%{F#e0e0e0}▄"
 elif [ "$capacity" -lt 90 ]; then
-  # Red if battery < 50%
   echo "%{F#e0e0e0}▅"
 else
   # Default color (light gray)
