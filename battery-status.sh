1eb3d4dbf4c27964c9c7df730c17c0cb5a8fccc5
ownssec <ownssec@gmail.com>
2025-07-26 15:23:49 +0800

    battery updated

--- battery-status.sh
+++ battery-status.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+BAT_PATH="/sys/class/power_supply/BAT0"
+
+# Exit silently if no battery
+if [ ! -f "$BAT_PATH/capacity" ]; then
+  exit 0
+fi
+
 capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 
 if [ "$capacity" -lt 30 ]; then
@@ -7,12 +15,12 @@ if [ "$capacity" -lt 30 ]; then
   echo "%{F#ff5555}▮▯▯▯"
 elif [ "$capacity" -lt 60 ]; then
   # Red if battery < 50%
-  echo "%{F#e0e0e0}▮▮▯▯"
+  echo "%{F#e0e0e0}▂"
 elif [ "$capacity" -lt 90 ]; then
   # Red if battery < 50%
   echo "%{F#e0e0e0}▮▮▮▯"
 else
   # Default color (light gray)
   # echo "%{F#e0e0e0}% ▮▮▮▮ $capacity%%{F-} "
-  echo "%{F#e0e0e0}▮▮▮▮"
+  echo "%{F#e0e0e0}█"
 fi
