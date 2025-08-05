3a260d39315122a991e08525f2d84e32800c6910
ownssec <ownssec@gmail.com>
2025-08-05 13:49:37 +0800

    update icon and right click of the mic will show the pulse audio

--- check-mic.sh
+++ check-mic.sh
@@ -7,8 +7,22 @@ MIC_SOURCE=$(pactl info | grep "Default Source" | cut -d' ' -f3)
 IS_MUTED=$(pactl get-source-mute "$MIC_SOURCE" | awk '{print $2}')
 
 if [ "$IS_MUTED" = "yes" ]; then
-    echo "mic 0" 
+    echo "0" 
 else
-    echo "mic 1"
+    echo "1" 
 fi
+
+# #!/bin/bash
+#
+# # Get the default source (microphone)
+# MIC_SOURCE=$(pactl info | grep "Default Source" | cut -d' ' -f3)
+#
+# # Get mute status
+# IS_MUTED=$(pactl get-source-mute "$MIC_SOURCE" | awk '{print $2}')
+#
+# if [ "$IS_MUTED" = "yes" ]; then
+#     echo " " 
+# else
+#     echo "mic 1"
+# fi
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+<!-- for icons of polybar -->
+
+sudo apt install fonts-font-awesome
